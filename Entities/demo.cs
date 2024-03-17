9b3a094159ac7d0dcdfa9df5403511c4b0c4de09
Lương Vũ Đình Duy <21133018@student.hcmute.edu.vn>
2024-03-17 13:44:29 +0700

    Create demo

---
+++ Entities/demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grocery_store.Entities
+{
+    internal class demo
+    {
+        private string name;
+    }
+}
