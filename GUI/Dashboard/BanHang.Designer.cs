534969ccea7823ee8875ec52893226f7a53f6e39
Lương Vũ Đình Duy <21133018@student.hcmute.edu.vn>
2024-03-18 15:56:12 +0700

    Add: User Control
    - Add user controls for store functions
    - Set up the main interface of the form including function keys of each user control
    - Complete the basic interface of the dash board

---
+++ GUI/Dashboard/BanHang.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace grocery_store.GUI
+{
+    partial class BanHang
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(511, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bán hàng";
+            // 
+            // BanHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "BanHang";
+            this.Size = new System.Drawing.Size(1200, 870);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
--- GUI/Main.cs
+++ GUI/Main.cs
@@ -15,6 +15,8 @@ namespace grocery_store.GUI
         public Main()
         {
             InitializeComponent();
+            reset_UC();
+            UC_BanHang.Visible = true;
         }
 
         private void btn_close_Click(object sender, EventArgs e)
@@ -25,6 +27,66 @@ namespace grocery_store.GUI
         private void btn_minimize_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void reset_UC()
+        {
+            //set visible all user control is false
+            UC_BanHang.Visible = false;
+            UC_HoaDon.Visible = false;
+            UC_HangHoa.Visible = false;
+            UC_ThongKe.Visible = false;
+            UC_NhanVien.Visible = false;
+            UC_QuanLy.Visible = false; 
+        }
+
+        private void btn_banhang_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_BanHang.Visible = true;
+            btn_banhang.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+            
+        }
+
+        private void btn_hoadon_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_HoaDon.Visible = true;
+            btn_hoadon.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+        }
+
+        private void btn_hanghoa_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_HangHoa.Visible = true;
+            btn_hanghoa.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+        }
+
+        private void btn_thongke_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_ThongKe.Visible = true;
+            btn_thongke.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+        }
+
+        private void btn_nhanvien_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_NhanVien.Visible = true;
+            btn_nhanvien.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+        }
+
+        private void btn_quanly_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            UC_QuanLy.Visible = true;
+            btn_quanly.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
+        }
+
+        private void btn_thoat_Click(object sender, EventArgs e)
+        {
+            reset_UC();
+            btn_thoat.OnPressedState.FillColor = Color.FromArgb(82, 121, 111);
         }
     }
 }
