6e1d95d368f64b7229486fc96e7f92790adc06fd
Lương Vũ Đình Duy <21133018@student.hcmute.edu.vn>
2024-05-01 10:48:28 +0700

    UPDATE: Run application

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using grocery_store.API;
+using grocery_store.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace grocery_store
@@ -11,17 +16,31 @@ namespace grocery_store
         [STAThread]
         static void Main()
         {
-            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("vi-VI");
-            System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = new System.Globalization.CultureInfo("vi-VI");
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             //API.GenarateBarCode genarateBarCode = new API.GenarateBarCode();
             //genarateBarCode.Genarate();
             //API.PaymentQR paymentQR = new API.PaymentQR();
             //paymentQR.Genarate();
+            updatePayment();
             Application.Run(new GUI.Main());
         }
+
+        static async void updatePayment()
+        {
+            using(GroceryStoreContext db = new GroceryStoreContext())
+            {
+                Payment payment = await db.Payment.FindAsync(1);
+                if(payment.Qr == null)
+                {
+                    PaymentQR paymentQR = new PaymentQR();
+                    paymentQR.Genarate();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
     }
 
 }
